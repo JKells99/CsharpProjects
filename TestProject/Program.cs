63e0215313189ba03522a3e56deabceda1bdfcec
Jordan <jordan.kelloway@keyin.com>
2023-11-21 17:37:18 -0330

    First push

---
+++ TestProject/Program.cs
@@ -0,0 +1,64 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+bool triple;
+bool doubles; 
+
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if((roll1 == roll2 || (roll2==roll3) || (roll1 == roll3)))
+{
+    Console.WriteLine("you Rolled doubles! +2 Points");
+    total+=2;
+
+}
+
+if((roll1==roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You Rolled triples! +6 bonus");
+    total +=6;
+}
+
+
+
+if (total >= 15)
+{
+    Console.WriteLine("You Win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("You Suck Ass....");
+}
+
+
+
+
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
