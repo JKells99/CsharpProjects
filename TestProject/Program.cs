4bd9840a53c3095aa3b90b7d6ea46081b04088dc
Jordan <jordan.kelloway@keyin.com>
2023-12-03 12:17:59 -0330

    New Code From Microsoft Learn

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -48,41 +48,81 @@
 // }
 
 
-Random random = new Random();
-int daysUntilExpiry = random.Next(12);
-int discountPercentage = 0;
+// Random random = new Random();
+// int daysUntilExpiry = random.Next(12);
+// int discountPercentage = 0;
 
-if (daysUntilExpiry == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiry == 1)
-{
-    discountPercentage = 20;
-    Console.WriteLine($"Your subscription expires within a day!");
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-
-
-}
-else if (daysUntilExpiry <= 5)
-{
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiry} days.");
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+// if (daysUntilExpiry == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiry == 1)
+// {
+//     discountPercentage = 20;
+//     Console.WriteLine($"Your subscription expires within a day!");
+//     Console.WriteLine($"Renew now and save {discountPercentage}%!");
 
 
-}
-else if (daysUntilExpiry <= 10)
-{
+// }
+// else if (daysUntilExpiry <= 5)
+// {
+//     discountPercentage = 10;
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiry} days.");
+//     Console.WriteLine($"Renew now and save {discountPercentage}%!");
+
+
+// }
+// else if (daysUntilExpiry <= 10)
+// {
     
-    Console.WriteLine($"Your subscription will expire soon. Renew now!");
+//     Console.WriteLine($"Your subscription will expire soon. Renew now!");
     
 
-}
+// }
 
-if(discountPercentage > 0)
+// if(discountPercentage > 0)
+// {
+//     Console.WriteLine($"A Discount Has Been Applied! At {discountPercentage}%");
+// }
+
+// string[] fradulentOrdersIds = new string[3];
+
+// fradulentOrdersIds[0] = "A123";
+// fradulentOrdersIds[1] = "B456";
+// fradulentOrdersIds[2] = "C789";
+
+
+
+// string[] fradulentOrdersIds = {"A123","B456","C789"};
+
+// Console.WriteLine($"FIRST: {fradulentOrdersIds[0]}");
+// Console.WriteLine($"SECOND: {fradulentOrdersIds[1]}");
+// Console.WriteLine($"THIRD: {fradulentOrdersIds[2]}");
+
+// fradulentOrdersIds[0] = "G568";
+
+// Console.WriteLine(" ");
+
+// Console.WriteLine($"FIRST: {fradulentOrdersIds[0]}");
+// Console.WriteLine($"SECOND: {fradulentOrdersIds[1]}");
+// Console.WriteLine($"THIRD: {fradulentOrdersIds[2]}");
+
+// Console.WriteLine($"There are {fradulentOrdersIds.Length} Fraudulent Orders to Process");
+
+// string[] names = {"Rowena", "Robin","Bao"};
+// foreach(string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+
+int[] inventory = {200,450,700,175,250};
+int itemNumber = 1;
+
+foreach(int items in inventory)
 {
-    Console.WriteLine($"A Discount Has Been Applied! At {discountPercentage}%");
+    Console.WriteLine($"Item {itemNumber}: {items}");
+    itemNumber++;
 }
 
 
